af61640f2abae9451d28ed45a94f1366d0d4b8eb
Gregory Moon <grmoon@asu.edu>
2015-04-27 00:02:28 -0700

    final

--- makefile
+++ makefile
@@ -1,9 +1,11 @@
-all: LRU.o main.o
-	g++ -std=c++11 -o LRU LRU.o main.o
+all: LRU.o main.o ARC.o
+	g++ -std=c++11 -o main LRU.o main.o ARC.o
 main.o: main.cpp
 	g++ -std=c++11 -c main.cpp
 LRU.o: LRU.h LRU.cpp
 	g++ -std=c++11 -c LRU.h LRU.cpp
+ARC.o: ARC.h ARC.cpp LRU.cpp LRU.h
+	g++ -std=c++11 -c ARC.h ARC.cpp LRU.cpp LRU.h
 clean:
 	rm *.o
 	rm *.gch
--- run-inputs.sh
+++ run-inputs.sh
@@ -6,34 +6,34 @@ for entry in "$DIR"/*
 do
 	printf "\n$entry\n--------------\n\n"
 	printf "cache-size = 1024\n"
-	./LRU 1024 "$entry"
+	./main 1024 "$entry"
 
 	printf "\ncache-size = 2048\n"
-	./LRU 2048 "$entry"
+	./main 2048 "$entry"
 
 	printf "\ncache-size = 4096\n"
-	./LRU 4096 "$entry"
+	./main 4096 "$entry"
 
 	printf "\ncache-size = 8192\n"
-	./LRU 8192 "$entry"
+	./main 8192 "$entry"
 
 	printf "\ncache-size = 16384\n"
-	./LRU 16384 "$entry"
+	./main 16384 "$entry"
 
 	printf "\ncache-size = 32768\n"
-	./LRU 32768 "$entry"
+	./main 32768 "$entry"
 
 	printf "\ncache-size = 65536\n"
-	./LRU 65536 "$entry"
+	./main 65536 "$entry"
 
 	printf "\ncache-size = 131072\n"
-	./LRU 131072 "$entry"
+	./main 131072 "$entry"
 
 	printf "\ncache-size = 262144\n"
-	./LRU 262144 "$entry"
+	./main 262144 "$entry"
 
 	printf "\ncache-size = 524288\n"
-	./LRU 524288 "$entry"
+	./main 524288 "$entry"
 done
 
 wait
